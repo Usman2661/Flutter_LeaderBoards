de7bdce0a9915eefbc8d4cba87399a39b8779964
Usman Ali <usmanusman136@hotmail.com>
2020-09-29 13:18:20 +0100

    lock orientation to portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:leaderboards/screens/home.dart';
 import 'package:leaderboards/screens/loading.dart';
 
 void main(){
-     runApp(MaterialApp(
-    initialRoute: '/home',
-    routes: {
-      '/': (context) => Loading(),
-      '/home': (context) => Home(),
-    },
-  ));
+
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+    .then((_) {
+         runApp(MaterialApp(
+          initialRoute: '/home',
+          routes: {
+            '/': (context) => Loading(),
+            '/home': (context) => Home(),
+          },
+        ));
+    }); 
 }
