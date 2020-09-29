956c10f15dabfe89033e83ca569e9945b5bd9c7d
Usman Ali <usmanusman136@hotmail.com>
2020-09-29 16:10:12 +0100

    added helper color method

---
+++ lib/widgets/GamesList.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:leaderboards/helper/colorFromHEX.dart';
+
+class GamesList extends StatefulWidget {
+  @override
+  _GamesListState createState() => _GamesListState();
+}
+
+class _GamesListState extends State<GamesList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
